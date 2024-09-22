a95b85cb4cb23f32d7e9ccc135f0ba07c6d44760
Mohsen <mohsenmahoski@gmail.com>
2024-09-22 10:52:10 +0330

    feat: add docker file for docs

--- apps/docs/app/page.tsx
+++ apps/docs/app/page.tsx
@@ -3,7 +3,7 @@ import ClientWrapper from "@components/clientWrapper";
 export default function Home() {
   return (
     <div className="bg-gray-200 w-screen h-screen flex items-center justify-center">
-      <h1>WEB PAGE</h1>
+      <h1>DOCS PAGE</h1>
       <ClientWrapper />
     </div>
   );
